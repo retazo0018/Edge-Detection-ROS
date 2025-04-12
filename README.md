cab3c14325a6012b09d8fa3719ff654f87b9fb63
amurali <amu459@node-robotics.com>
2025-04-12 21:11:32 +0200

    chore: added comments, res video and update README.md, package.xml

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
-# Edge Detection
+# Edge Detection ROS
+This ROS package detects edges in a checkerboard image, projects those edges into 3D space, and visualizes them as markers for each TF frame.
 
+## Features
+- Implemented in ROS1 Noetic
+- Edge detection after artifacts removal
+- Projection of detected edges into 3D space using camera intrinsics
+- Visualization of 3D edges as RViz markers for each TF frame
+
+## Getting started
+
+### Basic
+- `edge_detection.py` contains the source code.
+- Required packages include opencv and numpy.
+
+### Vision_ROS
+- Start `roscore` on a terminal.
+- Intialise a ROS workspace and run `catkin_make` from root.
+- Install any missing ROS packages when prompted by `sudo apt update && sudo apt install ros-noetic-<package-name>`
+- Source the workspace `source <path to catkin workspace>/devel/setup.bash`.
+- Start the server/node using `rosrun edge_detection edge_detector.py`.
+
+- ROS Service
+    - Start the client using `rosrun edge_detection edge_detector_client.py "/<PATH_TO_IMAGES_DIRECTORY>/"`.
+    - A superimposed image with the detected edges highlighted in green is saved for each input image, inside a `results` subdirectory located within the same input directory.
+
+- Bagfile Input
+    - Play the bagfile by `rosbag play --clock -l <path to bagfile>`.
+    - The default values of the camera topics are listed below. Please modify otherwise.
+        - `camera_depth_aligned_pc_topic`: `/camera/depth/points`
+        - `camera_depth_image_topic`: `/camera/depth/image_rect_raw`
+        - `camera_rgb_image_topic`: `/camera/color/image_raw`
+    - Open RViz and change the fixed frame to `camera_color_optical_frame`.
+    - The edge image and corresponding point cloud are published to the `/edge_image` and `/edge_points` topics, respectively.
+
+### Robot_ROS
+- Start `roscore` on a terminal.
+- Source the workspace `source <path to catkin workspace>/devel/setup.bash`.
+- Start the node using `rosrun edge_detection edge_detector.py`.
+- Launch the robot using `rosparam set /use_sim_time true && roslaunch mira_picker display.launch gripper_name:=robotiq2f_140 publish_joint_state:=false publish_robot_state:=false`
+- Play the bagfile by `rosbag play --clock -l <path to bagfile>`.
+- Edges are visualized as markers in the `/edge_points_marker` topic. In RViz, set the fixed frame to any TF frame of your choice to view the markers in the desired frame.
+
+### Notes
+- The result videos are found in `results` directory.
+    - The video `results/result_camera_frame.mp4` visualizes the output of ROBOT_ROS package in `camera_color_optical_frame` only.
+    - The video `results/result_all_frames.mp4` visualizes the output of ROBOT_ROS package in a few arbitrary frames.
+- The results of sample images are found inside `data/results` directory.
--- src/edge_detector.py
+++ src/edge_detector.py
@@ -24,11 +24,15 @@ class edgeDetectionNode:
         self.bridge = CvBridge()
         self.fields = [PointField('x', 0, PointField.FLOAT32, 1),PointField('y', 4, PointField.FLOAT32, 1),
                         PointField('z', 8, PointField.FLOAT32, 1)]
+        # Source frame of detection
         self.source_frame = "camera_color_optical_frame"
         self.points_3d = np.array([])
         self.frames = []
+        # Downsamples detected edges for optimization
+        self.edge_downsampling = 10 
         
         try:
+            # Rosparams for camera topics
             camera_depth_aligned_pc_topic = rospy.get_param('~camera_depth_aligned_pc_topic', '/camera/depth/points')
             camera_depth_image_topic = rospy.get_param('~camera_depth_image_topic', '/camera/depth/image_rect_raw')
             camera_rgb_image_topic = rospy.get_param('~camera_rgb_image_topic', '/camera/color/image_raw')
@@ -37,7 +41,7 @@ class edgeDetectionNode:
 
         # Subscribers
         self.camera_info_sub = rospy.Subscriber('/camera/color/camera_info', CameraInfo, self.camera_info_callback)
-        points_sub = message_filters.Subscriber(camera_depth_aligned_pc_topic, PointCloud2, queue_size=10)
+        # points_sub = message_filters.Subscriber(camera_depth_aligned_pc_topic, PointCloud2, queue_size=10)
         depth_img_sub = message_filters.Subscriber(camera_depth_image_topic, Image, queue_size=10)
         rgb_img_sub = message_filters.Subscriber(camera_rgb_image_topic, Image, queue_size=10)
         
@@ -52,20 +56,22 @@ class edgeDetectionNode:
         #TF
         self.tf_buffer = tf2_ros.Buffer()
         self.tf_listener = tf2_ros.TransformListener(self.tf_buffer)
-        self.marker_timer = rospy.Timer(rospy.Duration(0.05), self.marker_timer_callback)
+        self.marker_timer = rospy.Timer(rospy.Duration(0.01), self.marker_timer_callback)
 
-        # Time Synchronizer
+        # Time Synchronizer for RGB and Depth Images
         self.ts = message_filters.ApproximateTimeSynchronizer([rgb_img_sub, depth_img_sub], queue_size=2, slop=0.01, allow_headerless=False)
         self.ts.registerCallback(self.main)
 
         rospy.spin()   
     
     def get_all_frames(self) -> list:
+        # Returns list of all available frames in the TF tree
         yaml_str = self.tf_buffer.all_frames_as_yaml()
         data = yaml.safe_load(yaml_str)
         return list(data.keys())
     
     def camera_info_callback(self, camera_info)-> None:
+        # Camera Parameters (Intrinsics)
         self.frames = self.get_all_frames()
         self.camera_info = camera_info
         self.camera_matrix = np.array(camera_info.K).reshape(3,3) 
@@ -74,22 +80,26 @@ class edgeDetectionNode:
         self.camera_info_sub.unregister()
 
     def detect_edges_srv(self, req):
+        # Service call
         try:
             img_path = req.img_path
             img = cv2.imread(img_path, cv2.IMREAD_COLOR) 
-            edges = self.detect_edges(img)
+            edges = self.detect_keypoints(img)
             height, width = edges.shape
             return EdgeDetectionResponse(info=True, height=height, width=width, data=edges.flatten().tolist())
         except:
             return EdgeDetectionResponse(info=False, height=0, width=0, data=[])
 
     @staticmethod
-    def detect_edges(rgb_image, isServiceCall=True) -> np.ndarray:
+    def detect_keypoints(rgb_image, isServiceCall=True) -> np.ndarray:
+        # Detect edges and contours in the input image
         img_gray = cv2.cvtColor(rgb_image, cv2.COLOR_BGR2GRAY)
+        # Noise removal
         img_blur = cv2.bilateralFilter(img_gray, d=9, sigmaColor=75, sigmaSpace=75)
         edges = cv2.Canny(image=img_blur, threshold1=100, threshold2=200) # Canny Edge Detection
         
         if isServiceCall:
+            # Return only edges for a service call
             return edges
         
         contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -101,6 +111,7 @@ class edgeDetectionNode:
     
     @staticmethod
     def get_edge_pixels(edges, contours) -> np.ndarray:
+        # Returns detected edges as pixels
         edge_points = np.column_stack(np.where(edges > 0))
         # Convert (row, col) to (x, y)
         edge_pixels = [(int(x), int(y)) for y, x in edge_points]
@@ -108,6 +119,7 @@ class edgeDetectionNode:
         if contours is None:
             return np.asarray(edge_pixels)
 
+        # Remove edges outside the checkerboard boundary
         largest_contour = max(contours, key=cv2.contourArea)
         hull = cv2.convexHull(largest_contour)
         # Filter edge pixels to keep only those inside the hull
@@ -119,6 +131,7 @@ class edgeDetectionNode:
         return np.asarray(filtered_edge_pixels)
     
     def edge_pixels_to_3d(self, edge_pixels, depth_image) -> np.ndarray:
+        # Convert 2D pixels to 3D points in a batch for optimization
         u = edge_pixels[:, 0]
         v = edge_pixels[:, 1]
         # Extract depth values at those pixels and convert to meters
@@ -133,6 +146,7 @@ class edgeDetectionNode:
         self.points_3d = np.stack((X, Y, Z), axis=-1)
 
     def publish_pointcloud_from_points(self) -> None:
+        # Publish 3D points as pointcloud
         header = Header()
         header.stamp = rospy.Time.now()
         header.frame_id = self.source_frame
@@ -140,9 +154,10 @@ class edgeDetectionNode:
         self.edge_pc_pub.publish(pc_msg)
     
     def marker_timer_callback(self, _) -> None:
+        # Callback to publisg 3D edge points as markers for each TF frame
         marker_sets = []
         if len(self.points_3d) != 0:
-            self.points_3d = self.points_3d[::15]
+            self.points_3d = self.points_3d[::self.edge_downsampling]
             for frame in self.frames:
                 if frame == self.source_frame:
                     continue
@@ -151,12 +166,13 @@ class edgeDetectionNode:
                 # Add homogeneous 1s for batch transform: (N, 4)
                 ones = np.ones((self.points_3d.shape[0], 1))
                 pts_hom = np.hstack([self.points_3d, ones])
-                # Transform all points at once: (N, 4) x (4, 4)^T
+                # Optimization: Transform all points at once: (N, 4) x (4, 4)^T
                 transformed_pts = (tf_matrix @ pts_hom.T).T[:, :3]
                 marker_sets.append((transformed_pts, frame))
         self.publish_edge_markers(marker_sets)
     
     def transform_to_matrix(self, transform) -> np.ndarray:
+        # Returns 4*4 transformation matrix from transform
         trans = transform.transform.translation
         rot = transform.transform.rotation
         translation = [trans.x, trans.y, trans.z]
@@ -166,6 +182,7 @@ class edgeDetectionNode:
         return tf_mat
 
     def publish_edge_markers(self, marker_sets) -> None:
+        # Publishes 3D edge points as MarkerArray for Optimization
         marker_array = MarkerArray()
         marker_id = 0
         for points_3d, frame_id in marker_sets:
@@ -190,11 +207,12 @@ class edgeDetectionNode:
         self.edge_marker_array_pub.publish(marker_array)
 
     def main(self, rgb_img, depth_msg) -> None:
+        # Main Function
         rospy.loginfo("Received synchronized messages.")
         rgb_image = self.bridge.imgmsg_to_cv2(rgb_img, "rgb8")
         depth_image = self.bridge.imgmsg_to_cv2(depth_msg, "passthrough")
         try:
-            edges, corners = self.detect_edges(rgb_image, isServiceCall=False)
+            edges, corners = self.detect_keypoints(rgb_image, isServiceCall=False)
             edge_pixels = self.get_edge_pixels(edges, corners)
             self.edge_pixels_to_3d(edge_pixels, depth_image)
 
