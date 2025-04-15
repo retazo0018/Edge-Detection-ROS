afc3a8835550cb854acbff8190e8f6e32bb2a1d9
amurali <amu459@node-robotics.com>
2025-04-15 15:39:16 +0200

    chore: added time sync and camera_info callback

--- include/edge_detection/EdgeDetector.hpp
+++ include/edge_detection/EdgeDetector.hpp
@@ -1,6 +1,13 @@
 #pragma once
 
 #include <ros/ros.h>
+#include <sensor_msgs/Image.h>
+#include <sensor_msgs/CameraInfo.h>
+#include <opencv2/opencv.hpp>
+#include <cv_bridge/cv_bridge.h>
+#include <message_filters/subscriber.h>
+#include <message_filters/time_synchronizer.h>
+#include <message_filters/sync_policies/approximate_time.h>
 
 namespace edge_detection {
 
@@ -8,9 +15,26 @@ class EdgeDetector {
 public:
     EdgeDetector(ros::NodeHandle& nh);
     void init();
+	void cameraInfoCallback(const sensor_msgs::CameraInfoConstPtr& msg);
+	void mainCallback(const sensor_msgs::ImageConstPtr& rgb_img,
+                      const sensor_msgs::ImageConstPtr& depth_img);
 
 private:
     ros::NodeHandle nh_;
+
+	ros::Subscriber camera_info_sub_;
+	sensor_msgs::CameraInfo camera_info_;
+	cv::Mat camera_matrix_;
+	cv::Mat dist_coeffs_;
+	double fx_, fy_, cx_, cy_;
+
+	message_filters::Subscriber<sensor_msgs::Image> rgb_sub;
+  	message_filters::Subscriber<sensor_msgs::Image> depth_sub;
+
+	typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::Image, sensor_msgs::Image> MySyncPolicy;
+	typedef message_filters::Synchronizer<MySyncPolicy> Sync;
+	boost::shared_ptr<Sync> sync_;
+
 };
 
 }  // namespace edge_detection
--- src/EdgeDetector.cpp
+++ src/EdgeDetector.cpp
@@ -5,15 +5,44 @@ using namespace edge_detection;
 
 namespace edge_detection {
 
-EdgeDetector::EdgeDetector(ros::NodeHandle& nh) : nh_(nh) {
-    // Constructor
-    ROS_INFO("EdgeDetector constructor called.");
-}
+	EdgeDetector::EdgeDetector(ros::NodeHandle& nh) 
+		: nh_(nh){
+		ROS_INFO("EdgeDetector constructor called.");
 
-void EdgeDetector::init() {
-    // Initialization logic goes here
-    ROS_INFO("EdgeDetector initialized.");
-}
+		rgb_sub.subscribe(nh_, "/camera/color/image_raw", 1);
+    	depth_sub.subscribe(nh_, "/camera/depth/image_rect_raw", 1);
+		sync_.reset(new Sync(MySyncPolicy(10), rgb_sub, depth_sub));
+    	sync_->registerCallback(boost::bind(&EdgeDetector::mainCallback, this, _1, _2));
+
+		camera_info_sub_ = nh_.subscribe("/camera/color/camera_info", 1, &EdgeDetector::cameraInfoCallback, this);
+	}
+
+	void EdgeDetector::cameraInfoCallback(const sensor_msgs::CameraInfoConstPtr& msg) {
+    	camera_info_ = *msg;
+		// Convert K to 3x3 matrix
+		camera_matrix_ = cv::Mat(3, 3, CV_64F);
+		for (int i = 0; i < 9; ++i) {
+			camera_matrix_.at<double>(i / 3, i % 3) = msg->K[i];
+		}
+		// Convert D (distortion coefficients)
+		dist_coeffs_ = cv::Mat(msg->D.size(), 1, CV_64F);
+		for (size_t i = 0; i < msg->D.size(); ++i) {
+			dist_coeffs_.at<double>(i, 0) = msg->D[i];
+		}
+		fx_ = camera_matrix_.at<double>(0, 0);
+		fy_ = camera_matrix_.at<double>(1, 1);
+		cx_ = camera_matrix_.at<double>(0, 2);
+		cy_ = camera_matrix_.at<double>(1, 2);
+
+		camera_info_sub_.shutdown();  // Unsubscribe after first callback
+		ROS_INFO("Camera intrinsics extracted and camera_info subscriber shut down.");
+	}
+
+	void EdgeDetector::mainCallback(const sensor_msgs::ImageConstPtr& rgb_img, const sensor_msgs::ImageConstPtr& depth_img) {
+		ROS_INFO("Received synchronized messages.");
+
+	}
+
 }
 
 int main(int argc, char** argv) {
@@ -21,7 +50,6 @@ int main(int argc, char** argv) {
     ros::NodeHandle nh;
 
     edge_detection::EdgeDetector detector(nh);
-    detector.init();
 
     ros::spin();
     return 0;
